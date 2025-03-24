47804e724ec8c15404827ae63b206370038fd886
Stanxnz <stanvdkooi@gmail.com>
2025-03-24 16:47:32 +0100

    in the API

--- Alchemy.mjs
+++ Alchemy.mjs
@@ -1,4 +1,27 @@
 import fetch from "node-fetch";
 
 const PLAYER_NAME = "stanv@uia.no";
+const ALCHEMY_API = "https://alchemy-kd0l.onrender.com/";
 
+(async function() {
+    const startUrl = `${ALCHEMY_API}start?player=${encodeURIComponent(PLAYER_NAME)}`;
+    const startResponse = await fetch(startUrl);
+    const startData = await startResponse.json();
+    console.log("Challenge started:", startData);
+})();
+
+async function submitAnswer(answer) {
+    const answerData = {
+        player: PLAYER_NAME,
+        answer: answer
+    };
+    const response = await fetch(`${ALCHEMY_API}answer`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(answerData)
+    });
+    const result = await response.json();
+    console.log("Answer response:", result);
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Exam-unit-3
+ Using the code from my previous assignment so I can easily get into the API by just changing the needed names. Was not as easy as espected, did not get a connection with the API after running my code. Forgot to add ();  to my calling function haha.
  
+ First challenge
+
